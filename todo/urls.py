78bd8c152acd0c760de103d5bc28b4f793e22350
adcc662 <adcc662@gmail.com>
2025-04-27 09:35:53 -0600

    Working with static files

--- todo/urls.py
+++ todo/urls.py
@@ -18,9 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf.urls.static import static
 from rest_framework import permissions
+from rest_framework.decorators import authentication_classes
 from strawberry.django.views import GraphQLView
 from todolist.schema import schema
+from django.conf import settings
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -38,8 +41,11 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('todolist.urls')),
-    path('graphql/', GraphQLView.as_view(schema=schema)),
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('graphql/', GraphQLView.as_view(schema=schema)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
